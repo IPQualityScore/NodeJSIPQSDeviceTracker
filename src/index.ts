8c9d34147f6ed210aa469cde5db58b9f3898cccb
Jon Welling <jon@jonwelling.com>
2023-10-17 11:21:29 -0700

    Updated package to accept custom domain in tracker

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ import {
 } from "./util/deviceFingerprintFunctions";
 import {addDeviceTrackingTags, addDeviceTrackingTagsAsync} from "./util/domManipulation";
 
-const initializeScript = (secretKey: string) => {
-    addDeviceTrackingTags(secretKey);
+const initializeScript = (secretKey: string, domain: string) => {
+    addDeviceTrackingTags(secretKey, domain);
 }
 
-const initializeScriptAsync = (secretKey: string) => {
-    return addDeviceTrackingTagsAsync(secretKey);
+const initializeScriptAsync = (secretKey: string, domain: string) => {
+    return addDeviceTrackingTagsAsync(secretKey, domain);
 }
 export default {
     initializeScript,
