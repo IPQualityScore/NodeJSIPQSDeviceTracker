081f9bdbf04ff2add97e969c71a145d2de8efb9d
Harry Zhang <zharry@users.noreply.github.com>
2025-09-05 02:05:26 +0000

    update AfterResult, AfterFailure, Store function parameters

--- lib/cjs/types/index.d.ts
+++ lib/cjs/types/index.d.ts
@@ -4,11 +4,11 @@ declare const _default: {
     initializeScriptCustom: (secretKey: string, customDomain: string, trackerName: string) => void;
     initializeScriptAsyncCustom: (secretKey: string, customDomain: string, trackerName: string) => Promise<void>;
     Init: () => void;
-    AfterResult: (resultCallback: () => void) => void;
-    AfterFailure: (resultCallback: () => void) => void;
+    AfterResult: (resultCallback: (result: any) => void) => void;
+    AfterFailure: (resultCallback: (result: any) => void) => void;
     SetFormFieldPrepend: (prefix: string) => void;
     Trigger: (anchorTag: string, eventCallback?: ((event: object) => void | undefined) | undefined) => void;
-    Store: (name: string, id: number) => void;
+    Store: (name: string, id: string) => void;
     Field: (fieldName: string, element: string) => void;
     Pause: () => void;
     Resume: () => void;
