a2d727f0927bd4b91201e07903d5b1dd94c4ceaf
jon welling <jonwelling@gmail.com>
2025-09-09 13:42:50 -0400

    resolved startup.store() bug where string values were rejected

--- lib/esm/types/util/deviceFingerprintFunctions.d.ts
+++ lib/esm/types/util/deviceFingerprintFunctions.d.ts
@@ -3,7 +3,7 @@ export declare const AfterResult: (resultCallback: () => void) => void;
 export declare const AfterFailure: (resultCallback: () => void) => void;
 export declare const SetFormFieldPrepend: (prefix: string) => void;
 export declare const Trigger: (anchorTag: string, eventCallback?: ((event: object) => void | undefined) | undefined) => void;
-export declare const Store: (name: string, id: number) => void;
+export declare const Store: (name: string, id: String) => void;
 export declare const Field: (fieldName: string, element: string) => void;
 export declare const Pause: () => void;
 export declare const Resume: () => void;
